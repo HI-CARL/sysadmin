53e4c4e8cd002e1f13d453ecbdf6c2880f911eb0
Dan Jones <dan@danneh.org>
2012-03-23 21:55:48 +0000

    adding backup scripts

---
+++ backup_mysql.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Dump MySQL databases to file. Designed to be run on daily crontab.
+
+##### Start Configuration #####
+MYSQL_HOST=
+MYSQL_PORT=3306
+MYSQL_USER=
+MYSQL_PASS=
+
+# no trailing / please
+BACKUP_LOCATION=/ebs/backup
+
+##### Finish Configuration #####
+
+rm $BACKUP_LOCATION/*.sql.gz
+
+echo "Backing up MySQL databases on $MYSQL_HOST"
+for DB in $(echo "show databases" | mysql -h$MYSQL_HOST -P$MYSQL_PORT -u$MYSQL_USER -p$MYSQL_PASS | grep -v Database | grep -v mysql | grep -v innodb | grep -v information_schema | grep -v test)
+do
+    echo "Dumping $DB to $BACKUP_LOCATION/${DB}.sql.gz"
+    mysqldump -h$MYSQL_HOST -P$MYSQL_PORT -u$MYSQL_USER -p$MYSQL_PASS --hex-blob $DB | gzip -c -9 > $BACKUP_LOCATION/${DB}.sql.gz
+done
+echo "Done."
